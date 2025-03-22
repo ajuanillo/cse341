60c917eafe984cdd0598c741c9ea5f9e8dccc483
ajuanillo <ajuanillo@byupathway.edu>
2025-03-22 10:19:59 -0500

    Professionals

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,25 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+
 const home = ((req, res, next) => {
     res.json('You are at home')
 });
 
-const personData = ((req, res, next) => {
-    res.json('Data of person')
-});
+const getAll = async (req, res) => {
+    const result = await mongodb.getDb().db().collection('contacts').find();
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
+}
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+}
 
-module.exports = { home, personData };
+module.exports = { home, getAll, getSingle };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 
 const myController = require('../controllers');
 routes.get('/', myController.home);
-routes.get('/professional', myController.personData);
+routes.get('/professional', myController.getSingle);
+routes.get('/professionals', myController.getAll);
 
 module.exports = routes;
